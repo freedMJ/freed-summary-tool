dd388d741e25b8c05f5d1a8ec847c80eb36fca8f
freedMJ <1071705638@qq.com>
2021-06-23 00:54:31 +0800

    init dev

---
+++ spring-design-patterns-practice/src/main/java/com/lmj/practice/SpringDesignPatternsPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.lmj.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDesignPatternsPracticeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDesignPatternsPracticeApplication.class, args);
+    }
+
+}
